a7c2abef74f6c81b81a9a4697e4f2d0c008ad926
pkhungs <pkhung0123@gmail.com>
2026-01-11 17:49:27 +0700

    Display User member in the page

---
+++ quan_ly_dien_gia_dung/src/java/model/Role.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author hung
+ */
+public class Role {
+    private int roleId;
+    private String roleName;
+    private String roleDescription;
+    private boolean active;
+    private Timestamp createdAt;
+
+    public Role() {
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleDescription() {
+        return roleDescription;
+    }
+
+    public void setRoleDescription(String roleDescription) {
+        this.roleDescription = roleDescription;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    
+}
