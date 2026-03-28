fcd873ae7c5ec0a275a7465e387b222b08de99d7
tson2809 <thaison28092004@gmail.com>
2026-03-28 10:12:12 +0700

    chore: apply latest local updates

--- quan_ly_dien_gia_dung/src/java/model/AuditLog.java
+++ quan_ly_dien_gia_dung/src/java/model/AuditLog.java
@@ -1,7 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package model;
 
 import java.util.Date;
 
+/**
+ *
+ * @author thais
+ */
 public class AuditLog {
 
     private long logId;
